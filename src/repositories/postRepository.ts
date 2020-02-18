4d216618e650908d8d11210451bab60920fa5d38
Erick Rivas Gómez <rivaserick@outlook.com>
2020-02-18 11:59:03 -0600

    Accept both changes in postRepository

--- src/repositories/postRepository.ts
+++ src/repositories/postRepository.ts
@@ -2,18 +2,15 @@ import Post from '../models/post'
 import Comment from '../models/comment'
 import { postController } from '../controllers/postController'
 
-<<<<<<< HEAD
 const savePost = async (post) => {
     let newPost = new Post(post)
     return await newPost.save()
 }
-=======
 const commentAPost = async (id, comment) => {
     const newComment = new Comment(comment)
     return await newComment.save()
 }
 
->>>>>>> bae977bcad2534730d04dd6ffcf6a8b11ce41d68
 export default {
     savePost
 }

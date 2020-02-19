b02f18fc84e363144c5bec47d730d07e54a77b7b
Erick Rivas Gómez <rivaserick@outlook.com>
2020-02-19 14:28:45 -0600

    Ordered post module

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express'
 import jwt from 'jsonwebtoken'
 import postRepository from '../repositories/postRepository'
-import { IPost } from '../models/comment'
 
 export const postController = Router()
 
 const checkToken = (req, res, next) => {
-    const token = req.headers['authorization']
+    const token = req.headers.authorization
 
     jwt.verify(token, 'super-key-super-secret', (err, data) => {
         if (err) {
@@ -19,25 +18,12 @@ const checkToken = (req, res, next) => {
 }
 
 postController.post('/', checkToken, async (req, res) => {
-    const decodedToken = jwt.decode(req.headers['authorization'])
+    const decodedToken = jwt.decode(req.headers.authorization)
     const post = await postRepository.savePost(req.body, decodedToken.email)
     if (post) {
         res.status(200).json({ message: 'OK', post })
     } else {
         res.status(400).json({ message: 'bad request', post })
-    }
-})
-
-
-
-postController.delete('/:id', checkToken, async (req, res) => {
-    const id = req.params.id
-    const post = await postRepository.findByIdAndDelete(id)
-    if (post) {
-        res.status(200).json({ message: "OK" })
-
-    } else {
-        res.status(404).json({ message: 'post not found' })
     }
 })
 
@@ -52,10 +38,23 @@ postController.get('/:id', checkToken, async (req, res) => {
     }
 })
 
+// Patch
+
+postController.delete('/:id', checkToken, async (req, res) => {
+    const id = req.params.id
+    const post = await postRepository.findByIdAndDelete(id)
+    if (post) {
+        res.status(200).json({ message: "OK" })
+
+    } else {
+        res.status(404).json({ message: 'post not found' })
+    }
+})
+
 postController.post('/:id/comment', checkToken, async (req, res) => {
     const id = req.params.id
-    
-    const decodedToken = jwt.decode(req.headers['authorization'])
+
+    const decodedToken = jwt.decode(req.headers.authorization)
     const post = await postRepository.commentPost(id, req.body, decodedToken.email)
     if (post) {
         res.status(200).json({ message: 'OK', post })
--- src/repositories/postRepository.ts
+++ src/repositories/postRepository.ts
@@ -1,54 +1,49 @@
 import Post from '../models/post'
-import jwt from 'jsonwebtoken'
-import  { IPost, Comment } from '../models/comment'
-import { postController } from '../controllers/postController'
+import { IPost, Comment } from '../models/comment'
 import mongoose from 'mongoose'
 import User from '../models/user'
 
 const savePost = async (body, email) => {
-    const author = User.findOne({email})
+    const author = User.findOne({ email })
     const newPost = new Post({
-        _id : new mongoose.Types.ObjectId(),
+        _id: new mongoose.Types.ObjectId(),
         title: body.title,
-        date : body.date,
-        author : (await author)._id,
-        comments : [],
+        date: body.date,
+        author: (await author)._id,
+        comments: [],
         totalComments: body.totalComments,
         content: body.content
     })
 
     return await newPost.save()
 }
+
+const findById = async (id) => {
+    return await Post.findById(id)
+}
+
+// Patch
+
+const findByIdAndDelete = async (id) => {
+    return await Post.findByIdAndDelete(id)
+}
+
 const commentPost = async (id, body, email) => {
-    const post : IPost= <IPost>(await findById(id))
-    //const post = Post.findById(id)
-    const author = User.findOne({email})
+    const post: IPost = (await findById(id)) as IPost
+    // const post = Post.findById(id)
+    const author = User.findOne({ email })
     const newComment = new Comment({
-        _id : new mongoose.Types.ObjectId(),
+        _id: new mongoose.Types.ObjectId(),
         comment: body.comment,
-        author : (await author)._id,
-        date : body.date,
+        author: (await author)._id,
+        date: body.date,
     })
     post.comments.push(newComment)
     post.totalComments += 1
     post.save()
-    
+
     return await newComment.save()
 }
-
-const findByIdAndDelete = async (id) =>{
-    return await Post.findByIdAndDelete(id)
-}
-//patch a post
-
-
-const findById = async (id) =>{
-    return await Post.findById(id)
-}
-
-//get a post
-
-
 
 export default {
     savePost,
